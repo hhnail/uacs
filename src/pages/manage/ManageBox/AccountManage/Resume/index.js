a9d5306af56d5bc869ba894fa43846e2a5e4282d
Hhnail <779943749@qq.com>
2021-10-31 22:14:08 +0800

    [调整]调整用户个人简历文件位置和UI结构；周末结束，今晚先写到这里吧~

--- src/pages/manage/ManageBox/AccountManage/Resume/index.js
+++ src/pages/manage/ManageBox/AccountManage/Resume/index.js
@@ -1,49 +1,127 @@
-import React, {useEffect, useRef} from 'react'
-import * as echarts from 'echarts'
+import React from 'react'
+import {Anchor, Avatar, Badge, Card, Cascader, DatePicker, Descriptions, Input, Row, Slider} from 'antd';
+import {MAJORANDCLASS} from '../../../../../constants/baseInfo'
+import {EditOutlined, EllipsisOutlined, SettingOutlined} from '@ant-design/icons';
 
-export default function Resume() {
 
-    const echartsContainerRef = useRef()
+// antd组件结构
+const {TextArea} = Input;
+const {Link} = Anchor;
+const {Meta} = Card;
 
-    const drawBar = () => {
-        // 基于准备好的dom，初始化echarts实例
-        const myChart = echarts.init(echartsContainerRef.current);
 
-        // 指定图表的配置项和数据
-        const option = {
-            title: {
-                text: '销量图'
-            },
-            tooltip: {},
-            legend: {
-                data: ['销量']
-            },
-            xAxis: {
-                data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-            },
-            yAxis: {},
-            series: [
-                {
-                    name: '销量',
-                    type: 'bar',
-                    data: [5, 20, 36, 10, 10, 20]
-                }
-            ]
-        };
-        myChart.setOption(option);
+export default function UserInfo() {
+
+    const handleDateChange = (value) => {
+        console.log(value);
     }
 
-    useEffect(() => {
-        drawBar()
-    }, [])
-
     return (
-        <div>
-            <div ref={echartsContainerRef} style={{
-                width: '100%',
-                height: 400,
-                marginTop: 30,
-            }}></div>
+        <div style={{display: "flex"}}>
+            {/*  =====  TODO 锚点效果 ===== */}
+            <div>
+                <Anchor>
+                    <b>简历目录</b>
+                    <Link href="#components-anchor-demo-basic" title="个人信息"/>
+                    <Link href="#components-anchor-demo-static" title="个人优势"/>
+                    <Link href="#components-anchor-demo-static" title="大学期望"/>
+                    <Link href="#components-anchor-demo-static" title="校园经历"/>
+                    <Link href="#API" title="哈哈哈哈">
+                        <Link href="#Anchor-Props" title="嘻嘻嘻嘻嘻"/>
+                        <Link href="#Link-Props" title="嘿嘿嘿嘿"/>
+                    </Link>
+                </Anchor>
+            </div>
+            <div>
+                <Descriptions title="" bordered={true} column={2}>
+                    <Descriptions.Item label="姓名">
+                        <Input placeholder="请填写姓名"/>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="性别">
+                        <Input placeholder="请填写性别"/>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="联系方式">
+                        <Input placeholder="请填写联系方式"/>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="兴趣爱好" span={2}>
+                        <Input placeholder="请填写兴趣爱好"/>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="学院/专业/班级">
+                        <Cascader options={MAJORANDCLASS} defaultValue={['学院', '专业', '班级']}
+                                  onChange={(value) => {
+                                      console.log(value);
+                                  }}
+                        />
+                    </Descriptions.Item>
+                    <Descriptions.Item label="生日">
+                        <DatePicker onChange={handleDateChange} bordered={false} placeholder="请选择生日" locale/>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="自我评价" span={2}>
+                        沟通能力：<Slider defaultValue={80} disabled={false}/>
+                        表达能力：<Slider defaultValue={80} disabled={false}/>
+                        合作能力：<Slider defaultValue={80} disabled={false}/>
+                        抗压能力：<Slider defaultValue={80} disabled={false}/>
+                        <TextArea rows={4}/>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="加入社团的期望" span={2}>
+                        <TextArea rows={4}/>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="面试结果评价">
+                        <Input placeholder="请填写兴趣爱好"/>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="纳新审核结果">
+                        <Badge status="processing" text="审核中"/>
+                    </Descriptions.Item>
+                </Descriptions>
+            </div>
+
+            <div>
+                <Row>
+                    <Card
+                        style={{width: 300}}
+                        cover={
+                            <img
+                                alt="example"
+                                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                            />
+                        }
+                        actions={[
+                            <SettingOutlined key="setting"/>,
+                            <EditOutlined key="edit"/>,
+                            <EllipsisOutlined key="ellipsis"/>,
+                        ]}
+                    >
+                        <Meta
+                            avatar={<Avatar src="https://joeschmoe.io/api/v1/random"/>}
+                            title="Card title"
+                            description="This is the description"
+                        />
+                    </Card>
+                </Row>
+
+                <Row>
+                    <Card
+                        style={{width: 300}}
+                        cover={
+                            <img
+                                alt="example"
+                                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                            />
+                        }
+                        actions={[
+                            <SettingOutlined key="setting"/>,
+                            <EditOutlined key="edit"/>,
+                            <EllipsisOutlined key="ellipsis"/>,
+                        ]}
+                    >
+                        <Meta
+                            avatar={<Avatar src="https://joeschmoe.io/api/v1/random"/>}
+                            title="Card title"
+                            description="This is the description"
+                        />
+                    </Card>
+                </Row>
+            </div>
         </div>
     )
 }
